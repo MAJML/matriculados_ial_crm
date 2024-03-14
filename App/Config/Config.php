7eeaa7abd0e04dd57c2a6658457a8d9aade3b0b3
ialsistemas <sistemas@arzobispoloayza.edu.pe>
2024-03-14 17:18:58 -0500

    subida de nuevo proyecto

---
+++ App/Config/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Config;
+
+class Config
+{
+    const DB_HOST    = '127.0.0.1';
+    const DB_PORT    = '3306';
+    const DB_NAME    = 'ial_easycrm';
+    const DB_USER    = 'ialeasycrm';
+    const DB_PASS    = '14L345yCRMf0x';
+    const DB_CHARSET = 'utf8mb4';
+}
